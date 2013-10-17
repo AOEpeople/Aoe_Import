6bc64d7351fdf9b47f905c69094acca60c31c3d7
Lee Saferite <lee.saferite@aoe.com>
2013-10-17 12:21:37 -0400

    Add exception used to indicate early termination from processor loop for single element

--- app/code/community/Aoe/Import/Model/Importer/Xml.php
+++ app/code/community/Aoe/Import/Model/Importer/Xml.php
@@ -147,6 +147,14 @@ class Aoe_Import_Model_Importer_Xml extends Aoe_Import_Model_Importer_Abstract
                     )
                 );
             }
+        } catch (Aoe_Import_Model_Importer_Xml_SkipElementException $e) {
+            $this->message(
+                sprintf(
+                    "==> (%s) %s",
+                    $correlationIdentifier,
+                    $e->getMessage()
+                )
+            );
         } catch (Exception $e) {
             // we really should never get here because exception should be handled inside the processor
             $this->message(
